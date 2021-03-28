8c65b8eab999c9f0c10e2763a42c3cf25365c2f0
Saull Brandão <saull@outlook.com>
2021-03-28 09:00:26 -0300

    Create a component to show the current date and time

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import CurrentDayCard from './components/CurrentDayCard'
+import DateTime from './components/DateTime'
 import api from './services/api'
 
 const App = () => {
   return (
     <div>
       <h1>Weather Forecast</h1>
+      <DateTime />
       <CurrentDayCard />
     </div>
   );
---
+++ src/components/DateTime.js
@@ -0,0 +1,11 @@
+import '../styles/dateTime.css'
+
+const DateTime = () => {
+    return (
+        <p className="date-time">
+            {`1:05 PM, Sun, Apr 10, 2021`}
+        </p>
+    )
+}
+
+export default DateTime
