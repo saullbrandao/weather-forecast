118912741c3d3a76924b297e02a5d05f145ddd3c
Saull Brandão <saull@outlook.com>
2021-03-28 09:53:58 -0300

    Add api call to GET the weather information and store in state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import React, { useEffect, useState } from 'react'
 import CurrentDayCard from './components/CurrentDayCard'
 import DateTime from './components/DateTime'
 import api from './services/api'
 
 const App = () => {
+  const [weather, setWeather] = useState({})
+
+  useEffect(() => {
+    api.get('data/2.5/onecall', {
+      params: {
+        lon: -38.554790,
+        lat: -3.788670,
+      }
+    }).then(response => setWeather(response.data))
+  }, [])
   return (
     <div>
       <h1>Weather Forecast</h1>
