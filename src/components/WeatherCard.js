1598c444ad5a212efc7099820ff2d4c225850f66
Saull Brandão <saull@outlook.com>
2021-04-03 11:37:42 -0300

    Use styled components for the elements of the WeatherCard component

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,28 +1,65 @@
 import styled from 'styled-components'
 
 const Card = styled.div`
-    display: grid;
-    width: 120px;
-    grid-template-rows: 80x 80px 80px;
+    display: flex;
+    flex-direction: column;
+    border-radius: 3px;
     align-items: center;
-    justify-items: center;
-    border: 1px solid;
+    background-color:#2E8BC0;
+    width: 10rem;
+    gap: 1rem;
+    padding: 4px;
+    font-size: 2rem;
+
 `
 
+const CurrentDayCard = styled(Card)`
+    background-color: #145DA0;
+`
+
+const Temperature = styled.p`
+    font-weight: bold;
+
+    sup {
+        font-size: 1rem;
+    }
+`
+
+const Icon = styled.img`
+    
+`
+
+const Humidity = styled.p`
+    font-size: 1rem;
+    font-weight:bold;
+`
+
+
 const WeatherCard = ({ dailyWeather, current }) => {
+    const iconURL = `https://openweathermap.org/img/w/${dailyWeather.weather[0].icon}.png`
+
     if (dailyWeather) {
         const date = new Date(dailyWeather.dt * 1000)
         const formatedDate = date.toDateString().slice(0, 4)
-
         return (
-            <Card>
-                <h3>{current ? 'Today' : formatedDate}</h3>
-                <p>{dailyWeather.weather[0].description}</p>
-                <div className="humidity">
-                    <p>Humidity</p>
-                    <p>{dailyWeather.humidity + '%'}</p>
-                </div>
-            </Card>
+            current ? <CurrentDayCard>
+                <h2>Today</h2>
+                <Temperature>{Math.floor(dailyWeather.temp)}<sup>°C</sup> </Temperature>
+                <Icon src={iconURL} />
+                <Humidity>
+                    Humidity {`${dailyWeather.humidity}%`}
+                </Humidity>
+
+            </CurrentDayCard>
+                : <Card>
+                    <h2>{formatedDate}</h2>
+                    <Temperature>{Math.floor(dailyWeather.temp.day)}<sup>°C</sup> </Temperature>
+
+                    <img src={iconURL} />
+                    <Humidity>
+                        <p>Humidity {`${dailyWeather.humidity}%`}</p>
+                    </Humidity>
+                </Card>
         )
     }
 
