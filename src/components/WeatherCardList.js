014ec8cefa6eb2940141fde639e3980287400630
Saull Brandão <saull@outlook.com>
2021-04-03 11:40:33 -0300

    Remove comments and implement the logic to select the current day

--- src/components/WeatherCardList.js
+++ src/components/WeatherCardList.js
@@ -4,20 +4,17 @@ import styled from 'styled-components'
 const CardList = styled.div`
     display: flex;
     flex-direction: row;
+    gap: 1rem;
 `
 
 const WeatherCardList = ({ weather }) => {
     return (
         <CardList>
-            {/* <WeatherCard dailyWeather={weather.current} current={true} /> */}
             {weather.daily ? weather.daily.map((day, index) => {
-                return index !== 0 && index < 7 ? <WeatherCard key={index} dailyWeather={day} current={false} /> : null
-            }) : <p>ABC</p>}
+                return index < 7 ? <WeatherCard key={index} dailyWeather={index === 0 ? weather.current : day} current={index === 0 ? true : false} /> : null
+            }) : <p>Loading</p>}
         </CardList>
     )
-    // return (for (let i = 1; i < weather.daily.length; i++) {
-    //     <WeatherCard />
-    // })
 }
 
 export default WeatherCardList
